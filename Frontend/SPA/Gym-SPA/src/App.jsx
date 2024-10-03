3eee2a3546827e79322260c844b2412894b331e0
Mahmoud-Ramzi5 <mzramzi5@gmail.com>
2024-10-03 15:29:21 +0300

    Responsive SPA

--- Frontend/SPA/Gym-SPA/src/App.jsx
+++ Frontend/SPA/Gym-SPA/src/App.jsx
@@ -1,18 +1,35 @@
-import { useState } from 'react'
-import { CgGym, CgCheck } from 'react-icons/cg';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import {
+  FaDumbbell, FaCheck, FaLocationDot,
+  FaPhone, FaEnvelope
+} from 'react-icons/fa6';
 import cat1 from './assets/cat1.png'
 import cat2 from './assets/cat2.png'
 import team1 from './assets/team1.png'
 import team2 from './assets/team2.png'
 import team3 from './assets/team3.png'
-import icon1 from './assets/icon1.svg'
-import icon2 from './assets/icon2.svg'
-import icon3 from './assets/icon3.svg'
 import './App.css'
 
 function App() {
+  const navigation = [
+    { href: '#home', name: 'Home' },
+    { href: '#', name: 'About' },
+    { href: '#about', name: 'Training' },
+    { href: '#gallery', name: 'Gallery' },
+    { href: '#pricing', name: 'Pricing' },
+    { href: '#contact', name: 'Contact' },
+  ]
+
+  const training = [
+    { img: cat1, h3: 'Personal traning', p: 'GG' },
+    { img: cat2, h3: 'Group traning', p: 'GG' }
+  ]
+  const gallery = [
+    { img: team1, h5: 'Body Building', p: 'GG' },
+    { img: team2, h5: 'Muscle Gain', p: 'GG' },
+    { img: team3, h5: 'Weight Loss', p: 'GG' }
+  ]
+
   const pricing = [
     {
       plan: '1 Month',
@@ -52,135 +69,45 @@ function App() {
     }
   ]
 
+  const contact = [
+    { icon: <FaLocationDot className='icon' />, h3: 'Location', p: 'Damascus, Syria' },
+    { icon: <FaPhone className='icon' />, h3: 'Phone', p: '(+963) 958 295 285' },
+    { icon: <FaEnvelope className='icon' />, h3: 'Email', p: 'CoachAmr@gmail.com' }
+  ]
+
   return (
     <>
       <nav>
         <ul id="navigation">
-          <li><a href="#home">Home</a></li>
-          <li><a href="">About</a></li>
-          <li><a href="#training">Training</a></li>
-          <li><a href="#gallery">Gallery</a></li>
-          <li><a href="#pricing">Pricing</a></li>
-          <li><a href="#contact">Contact</a></li>
+          {navigation.map((n) => (
+            <li><a href={n.href}>{n.name}</a></li>
+          ))}
         </ul>
       </nav>
 
       <section id='home'>
-        <div className="container">
-          <div className="caption">
-            <span>Hi This is Coach Amr</span>
-            <h1>Gym Trainer</h1>
-          </div>
+        <div className="caption">
+          <span>Hi This is Coach Amr</span>
+          <h1>Gym Trainer</h1>
         </div>
       </section>
 
+      {/* Training*/}
       <section id='training'>
         <div className='container'>
           <div className='row'>
-            <div className='column'>
-              <div className='topic'>
-                <div className='topic-img'>
-                  <img src={cat1} alt="" />
-                  <div className='content-box'>
-                    <div className='content'>
-                      <h3>Personal traning</h3>
-                      <p>You’ll look at graphs and charts in Task One, how to approach the task and <br /> the language needed for a successful answer.</p>
-                      <a href="" className="btn">View Courses</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className='column'>
-              <div className='topic'>
-                <div className='topic-img'>
-                  <img src={cat2} alt="" />
-                  <div className='content-box'>
-                    <div className='content'>
-                      <h3>Group traning</h3>
-                      <p>You’ll look at graphs and charts in Task One, how to approach the task and <br /> the language needed for a successful answer.</p>
-                      <a href="" className="btn">View Courses</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section id='gallery'>
-        <div className='container'>
-          <div className='row'>
-            <div className='column'>
-              <div className="section-title">
-                <h2>What I Offer</h2>
-              </div>
-            </div>
-          </div>
-          <div className='row'>
-            <div className='column'>
-              <div className='box'>
-                <div className='icon'>
-                  <img src={team1} alt="" />
-                </div>
-                <div className='caption'>
-                  <h5><a href="">Body Building</a></h5>
-                  <p>You’ll look at graphs and charts in Task One, how to approach the task </p>
-                </div>
-              </div>
-            </div>
-            <div className='column'>
-              <div className='box'>
-                <div className='icon'>
-                  <img src={team2} alt="" />
-                </div>
-                <div className='caption'>
-                  <h5><a href="">Muscle Gain</a></h5>
-                  <p>You’ll look at graphs and charts in Task One, how to approach the task </p>
-                </div>
-              </div>
-            </div>
-            <div className='column'>
-              <div className='box'>
-                <div className='icon'>
-                  <img src={team3} alt="" />
-                </div>
-                <div className='caption'>
-                  <h5><a href="">Weight Loss</a></h5>
-                  <p>You’ll look at graphs and charts in Task One, how to approach the task </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      {/* Pricing */}
-      <section id='pricing'>
-        <div className='container'>
-          <div className='row'>
-            <div className='column'>
-              <div className='section-title'>
-                <h2>Pricing</h2>
-              </div>
-            </div>
-          </div>
-          <div className='row'>
-            {pricing.map((p) => (
+            {training.map((t) => (
               <div className='column'>
-                <div className='card'>
-                  <CgGym className='icon' />
-                  <div className='details'>
-                    <span className='plan'>{p.plan}</span>
-                    <p>{p.price}  <span>{p.span_txt}</span></p>
-                    {p.features.map((feature) => (
-                      <div className='features'>
-                        <CgCheck className='check' />
-                        <p>{feature}</p>
+                <div className='topic'>
+                  <div className='topic-img'>
+                    <img src={t.img} alt="" />
+                    <div className='content-box'>
+                      <div className='content'>
+                        <h3>{t.h3}</h3>
+                        <p>{t.p}</p>
+                        <a href="#" className="btn">View Courses</a>
                       </div>
-                    ))}
-                    <a href="#" className='button'>Join Now</a>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -189,49 +116,85 @@ function App() {
         </div>
       </section>
 
-      <section id='contact'>
+      {/* Gallery */}
+      <section id='gallery'>
         <div className='container'>
           <div className='row'>
-            <div className='column'>
-              <div className='item'>
-                <div className='icon'>
-                  <img src={icon1} alt="" />
-                </div>
-                <div className='caption'>
-                  <h3>Location</h3>
-                  <p>You’ll look at graphs and charts in Task One, how to approach </p>
+            <div className="section-title">
+              <h2>What I Offer</h2>
+            </div>
+          </div>
+          <div className='row'>
+            {gallery.map((g) => (
+              <div className='column'>
+                <div className='box'>
+                  <div className='box-img'>
+                    <img src={g.img} alt="" />
+                  </div>
+                  <div className='caption'>
+                    <h5>{g.h5}</h5>
+                    <p>{g.p}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className='column'>
-              <div className='item'>
-                <div className='icon'>
-                  <img src={icon2} alt="" />
-                </div>
-                <div className='caption'>
-                  <h3>Phone</h3>
-                  <p>(90) 277 278 2566</p>
-                  <p>  (78) 267 256 2578</p>
-                </div>
-              </div>
-            </div>
-            <div className='column'>
-              <div className='item'>
-                <div className='icon'>
-                  <img src={icon3} alt="" />
-                </div>
-                <div className='caption'>
-                  <h3>Email</h3>
-                  <p>Amrcoach@gmail.com</p>
-                  <p>coashAmr@gmail.com</p>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
 
-      <div style={{ height: '2000px' }}> K </div>
+      {/* Pricing */}
+      <section id='pricing'>
+        <div className='container'>
+          <div className='row'>
+            <div className='section-title'>
+              <h2>Pricing</h2>
+            </div>
+          </div>
+          <div className='row'>
+            {pricing.map((p) => (
+              <div className='column'>
+                <div className='card'>
+                  <FaDumbbell className='icon' />
+                  <div className='details'>
+                    <span className='plan'>{p.plan}</span>
+                    <p>{p.price}  <span>{p.span_txt}</span></p>
+                    {p.features.map((feature) => (
+                      <div className='features'>
+                        <FaCheck className='check' />
+                        <p>{feature}</p>
+                      </div>
+                    ))}
+                    <div className='button'>
+                      <a href="#" className='btn'>Join Now</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      {/* Contact */}
+      <section id='contact'>
+        <div className='container'>
+          <div className='row'>
+            {contact.map((c) => (
+              <div className='column'>
+                <div className='item'>
+                  {c.icon}
+                  <div className='caption'>
+                    <h3>{c.h3}</h3>
+                    <p>{c.p}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <div style={{ height: '1000px' }}> K </div>
 
     </>
   )
