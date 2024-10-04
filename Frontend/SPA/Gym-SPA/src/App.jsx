d13ca6e072f5d7627daae0c52eb501332c04f629
Mahmoud-Ramzi5 <mzramzi5@gmail.com>
2024-10-04 23:06:32 +0300

    SPA footer

--- Frontend/SPA/Gym-SPA/src/App.jsx
+++ Frontend/SPA/Gym-SPA/src/App.jsx
@@ -1,6 +1,8 @@
 import { useRef } from 'react';
 import {
-  FaBars, FaTimes, FaCheck
+  FaBars, FaTimes, FaCheck,
+  FaFacebookF, FaInstagram,
+  FaWhatsapp, FaTelegramPlane
 } from 'react-icons/fa';
 import {
   FaDumbbell, FaLocationDot,
@@ -9,7 +11,7 @@ import {
 import gym from './assets/gym.jpg'
 import cat1 from './assets/cat1.png';
 import cat2 from './assets/cat2.png';
-import caoch from './assets/coach.jpg';
+import coach from './assets/coach.jpg';
 import app from './assets/gym-app.jpg';
 import team1 from './assets/team1.png';
 import team2 from './assets/team2.png';
@@ -32,10 +34,16 @@ function App() {
   ]
 
   const about = [
-    { img: caoch, h3: '', p: '' },
-    { img: '', h3: 'Coach Amr:', p: 'a trustworthy coach with a certificate in human organs and chemical effects in the body specialize in reforming the body to the desired shape.' },
+    { img: coach, h3: '', p: '' },
+    {
+      img: '', h3: 'Coach Amr:',
+      p: 'a trustworthy coach with a certificate in human organs and chemical effects in the body specialize in reforming the body to the desired shape.'
+    },
     { img: app, h3: '', p: '' },
-    { img: '', h3: 'My app:', p: 'special app designed to give professional advices and workout plans and it also provides many nutritional programs to give you the best experince and results.' }
+    {
+      img: '', h3: 'My app:',
+      p: 'special app designed to give professional advices and workout plans and it also provides many nutritional programs to give you the best experince and results.'
+    }
   ]
 
   const training = [
@@ -95,157 +103,208 @@ function App() {
 
   return (
     <>
-      <nav className='nav' ref={navbar}>
-        <button className='hide-nav' onClick={toggleNavbar}>
-          <FaTimes />
+      <header>
+        {/* NavBar */}
+        <nav className='nav' ref={navbar}>
+          <button className='hide-nav' onClick={toggleNavbar}>
+            <FaTimes />
+          </button>
+          <img className='logo' src={gym} alt='Logo' />
+          <ul>
+            {navigation.map((n, i) => (
+              <li key={i} onClick={toggleNavbar}>
+                <a href={n.href}>{n.name}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <button className='show-nav' onClick={toggleNavbar}>
+          <FaBars />
         </button>
-        <img className='logo' src={gym} />
-        <ul>
-          {navigation.map((n, i) => (
-            <li key={i} onClick={toggleNavbar}>
-              <a href={n.href}>{n.name}</a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <button className='show-nav' onClick={toggleNavbar}>
-        <FaBars />
-      </button>
+      </header>
 
-      <section id='home'>
-        <div className="caption">
-          <span>Hi This is Coach Amr</span>
-          <h1>Gym Trainer</h1>
-        </div>
-      </section>
+      <main>
+        {/* Home */}
+        <section id='home'>
+          <div className="caption">
+            <span>Hi This is Amr Khalaf</span>
+            <h1>Gym Trainer</h1>
+          </div>
+        </section>
 
-            {/* About */}
-            <section id='about'>
-        <div className='container'>
-          <div className='row'>
-            {about.map((a, i) => (
-              <div key={i} className='column'>
-                <div className='sec'>
-                  <div className='sec-img'>
-                    <img src={a.img} alt="" />
-                    <div className='content-box'>
-                      <div className='content'>
-                        <h3>{a.h3}</h3>
-                        <p>{a.p}</p>
+        {/* About */}
+        <section id='about'>
+          <div className='container'>
+            <div className='row'>
+              {about.map((a, i) => (
+                <div key={i} className='column'>
+                  <div className='sec'>
+                    <div className='sec-img'>
+                      <img src={a.img} alt="" />
+                      <div className='content-box'>
+                        <div className='content'>
+                          <h3>{a.h3}</h3>
+                          <p>{a.p}</p>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-
-      {/* Training */}
-      <section id='training'>
-        <div className='container'>
-          <div className='row'>
-            {training.map((t, i) => (
-              <div key={i} className='column'>
-                <div className='topic'>
-                  <div className='topic-img'>
-                    <img src={t.img} alt="" />
-                    <div className='content-box'>
-                      <div className='content'>
-                        <h3>{t.h3}</h3>
-                        <p>{t.p}</p>
-                        <a href="#" className='btn'>View Courses</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-
-      {/* Gallery */}
-      <section id='gallery'>
-        <div className='container'>
-          <div className='row'>
-            <div className="section-title">
-              <h2>What I Offer</h2>
+              ))}
             </div>
           </div>
-          <div className='row'>
-            {gallery.map((g, i) => (
-              <div key={i} className='column'>
-                <div className='box'>
-                  <div className='box-img'>
-                    <img src={g.img} alt="" />
-                  </div>
-                  <div className='caption'>
-                    <h5>{g.h5}</h5>
-                    <p>{g.p}</p>
+        </section>
+
+        {/* Training */}
+        <section id='training'>
+          <div className='container'>
+            <div className='row'>
+              <div className="section-title">
+                <h2>Training</h2>
+              </div>
+            </div>
+            <div className='row'>
+              {training.map((t, i) => (
+                <div key={i} className='column'>
+                  <div className='topic'>
+                    <div className='topic-img'>
+                      <img src={t.img} alt="" />
+                      <div className='content-box'>
+                        <div className='content'>
+                          <h3>{t.h3}</h3>
+                          <p>{t.p}</p>
+                          <a href="#" className='btn'>View Courses</a>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-
-      {/* Pricing */}
-      <section id='pricing'>
-        <div className='container'>
-          <div className='row'>
-            <div className='section-title'>
-              <h2>Pricing</h2>
+              ))}
             </div>
           </div>
-          <div className='row'>
-            {pricing.map((p, i) => (
-              <div key={i} className='column'>
-                <div className='card'>
-                  <FaDumbbell className='icon' />
-                  <div className='details'>
-                    <span className='plan'>{p.plan}</span>
-                    <p>{p.price}  <span>{p.span_txt}</span></p>
-                    {p.features.map((feature, index) => (
-                      <div key={index} className='features'>
-                        <FaCheck className='check' />
-                        <p>{feature}</p>
+        </section>
+
+        {/* Gallery */}
+        <section id='gallery'>
+          <div className='container'>
+            <div className='row'>
+              <div className="section-title">
+                <h2>What I Offer</h2>
+              </div>
+            </div>
+            <div className='row'>
+              {gallery.map((g, i) => (
+                <div key={i} className='column'>
+                  <div className='box'>
+                    <div className='box-img'>
+                      <img src={g.img} alt="" />
+                    </div>
+                    <div className='caption'>
+                      <h5>{g.h5}</h5>
+                      <p>{g.p}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+
+        {/* Pricing */}
+        <section id='pricing'>
+          <div className='container'>
+            <div className='row'>
+              <div className='section-title'>
+                <h2>Pricing</h2>
+              </div>
+            </div>
+            <div className='row'>
+              {pricing.map((p, i) => (
+                <div key={i} className='column'>
+                  <div className='card'>
+                    <FaDumbbell className='icon' />
+                    <div className='details'>
+                      <span className='plan'>{p.plan}</span>
+                      <p>{p.price}  <span>{p.span_txt}</span></p>
+                      {p.features.map((feature, index) => (
+                        <div key={index} className='features'>
+                          <FaCheck className='check' />
+                          <p>{feature}</p>
+                        </div>
+                      ))}
+                      <div className='button'>
+                        <a href="#" className='btn'>Join Now</a>
                       </div>
-                    ))}
-                    <div className='button'>
-                      <a href="#" className='btn'>Join Now</a>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+
+        {/* Contact */}
+        <section id='contact'>
+          <div className='container'>
+            <div className='row'>
+              {contact.map((c, i) => (
+                <div key={i} className='column'>
+                  <div className='item'>
+                    {c.icon}
+                    <div className='caption'>
+                      <h3>{c.h3}</h3>
+                      <p>{c.p}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+      </main>
+
+      <footer>
+        {/* Footer */}
+        <div id='footer'>
+          <div className='container'>
+            <div className='top'>
+              <div className='row'>
+                <div className='column'>
+                  <div className='caption'>
+                    <div className='f-logo'>
+                      <a href='#'><img src={gym} alt="" /></a>
+                    </div>
+                    <div className='menu'>
+                      <ul>
+                        {navigation.map((n, i) => (
+                          <li key={i} onClick={toggleNavbar}>
+                            <a href={n.href}>{n.name}</a>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                    <div className='social'>
+                      <a href="#"><FaFacebookF /></a>
+                      <a href="#"><FaInstagram /></a>
+                      <a href="#"><FaWhatsapp /></a>
+                      <a href="#"><FaTelegramPlane /></a>
                     </div>
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
-        </div>
-      </section>
-
-      {/* Contact */}
-      <section id='contact'>
-        <div className='container'>
-          <div className='row'>
-            {contact.map((c, i) => (
-              <div key={i} className='column'>
-                <div className='item'>
-                  {c.icon}
-                  <div className='caption'>
-                    <h3>{c.h3}</h3>
-                    <p>{c.p}</p>
+            </div>
+            <div className='bottom'>
+              <div className='row'>
+                <div className='column'>
+                  <div className='copy-right'>
+                    Copyright ©<script>document.write(new Date().getFullYear());</script>2024 All rights reserved
                   </div>
                 </div>
               </div>
-            ))}
+            </div>
           </div>
         </div>
-      </section>
-
-      <div style={{ height: '1000px' }}> K </div>
-
+      </footer>
     </>
   );
 }
