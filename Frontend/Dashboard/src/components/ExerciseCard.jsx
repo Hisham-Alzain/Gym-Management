bc6f669d520c79ff163e27a0bd6461e08f10c853
Mahmoud-Ramzi5 <mzramzi5@gmail.com>
2024-10-28 20:03:27 +0300

    some css work

--- Frontend/Dashboard/src/components/ExerciseCard.jsx
+++ Frontend/Dashboard/src/components/ExerciseCard.jsx
@@ -36,7 +36,7 @@ const ExerciseCard = ({ ExerciseData }) => {
 
   const handleCancel = (event) => {
     event.preventDefault();
-    setNewDescription('');
+    setNewDescription(ExerciseData.description);
     setUpdating(false);
   }
 
@@ -68,21 +68,33 @@ const ExerciseCard = ({ ExerciseData }) => {
             <h3 className={styles.muscle}>Muscle: {ExerciseData.muscle}</h3>
           </div>
           {!updating && <p>Description: {ExerciseData.description}</p>}
-          {updating && <input className={styles.description} id='newDescription' type='text' placeholder='New description' onChange={handleDescription} value={newDescription} />}
+          {updating &&
+            <input
+              type='text'
+              id='newDescription'
+              className={styles.description}
+              placeholder='New description'
+              onChange={handleDescription}
+              value={newDescription}
+            />
+          }
         </div>
-        <div className={styles.second_column}>
+        <div className={styles.column}>
           <VideoPopUp Path={ExerciseData.video} />
           <UploadPopUp exercise_id={ExerciseData.exercise_id} />
           {!updating && <button className={styles.update_button} onClick={() => setUpdating(true)}>Update description</button>}
           {updating &&
-            <form className={styles.desc_form} onSubmit={(event) => handleUpdateDescription(event, ExerciseData.exercise_id)}>
-              <div className={styles.btn_holder}>
-                <button className={styles.submit} type='submit'>Submit</button>
-                <button className={styles.cancel} onClick={handleCancel}>Cancel</button>
-              </div>
+            <form
+              className={styles.btn_holder}
+              onSubmit={(event) => handleUpdateDescription(event, ExerciseData.exercise_id)}
+            >
+              <button className={styles.submit} type='submit'>Submit</button>
+              <button className={styles.cancel} onClick={handleCancel}>Cancel</button>
             </form>
           }
-          <button className={styles.delete_button} onClick={(event) => handleDeleteExercise(event, ExerciseData.exercise_id)}>Delete exercise</button>
+          <button className={styles.delete_button} onClick={(event) => handleDeleteExercise(event, ExerciseData.exercise_id)}>
+            Delete exercise
+          </button>
         </div>
       </div>}
     </div>
