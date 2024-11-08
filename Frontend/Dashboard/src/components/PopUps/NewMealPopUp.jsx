84a941c1334415e353f0d2f76b75d149fcc72790
Mahmoud-Ramzi5 <mzramzi5@gmail.com>
2024-11-08 17:30:09 +0300

    MealPopUp Localization

--- Frontend/Dashboard/src/components/PopUps/NewMealPopUp.jsx
+++ Frontend/Dashboard/src/components/PopUps/NewMealPopUp.jsx
@@ -1,138 +1,172 @@
 import { useState, useContext } from 'react';
-import Popup from 'reactjs-popup';
-import 'reactjs-popup/dist/index.css';
-import { LoginContext } from "../../utils/Contexts";
 import { useTranslation } from 'react-i18next';
+import Popup from 'reactjs-popup';
 import { FaPlus } from "react-icons/fa6";
+import { LoginContext } from "../../utils/Contexts";
 import { AddMeal } from '../../apis/DietsApis';
 import img_holder from '../../assets/noImage.jpg';
 import styles from '../../styles/PopUps/meal_popup.module.css';
 
 
 const NewMealPopUp = () => {
-    // Translations
-    const { t } = useTranslation('global');
-    // Context
-    const { accessToken } = useContext(LoginContext);
+  // Translations
+  const { t } = useTranslation('global');
+  // Context
+  const { accessToken } = useContext(LoginContext);
 
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
-    const [calories, setCalories] = useState('');
-    const [protein, setProtein] = useState('');
-    const [carbs, setcarbs] = useState('');
-    const [fat, setfat] = useState('');
-    const [thumbnailPath, setThumbnailPath] = useState('');
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [calories, setCalories] = useState('');
+  const [protein, setProtein] = useState('');
+  const [carbs, setcarbs] = useState('');
+  const [fat, setfat] = useState('');
+  const [thumbnailPath, setThumbnailPath] = useState('');
 
-    const handleNewMeal = (event) => {
-        event.preventDefault();
-        AddMeal(accessToken, name, description, calories, protein, carbs, fat, thumbnailPath).then((response) => {
-            if (response.status == 201) {
-                console.log('Added meal successfully');
-                window.location.reload();
-            } else {
-                console.log(response.data);
-            }
-        });
+  const handleNewMeal = (event) => {
+    event.preventDefault();
+    AddMeal(
+      accessToken,
+      name,
+      description,
+      calories,
+      protein,
+      carbs,
+      fat,
+      thumbnailPath
+    ).then((response) => {
+      if (response.status == 201) {
+        console.log('Added meal successfully');
+        window.location.reload();
+      } else {
+        console.log(response.data);
+      }
+    });
+  }
+
+  const handlePhotoChange = (event) => {
+    const image = event.target.files[0];
+    const allowedImageTypes = ["image/png", "image/jpg", "image/jpeg"];
+    if (image && allowedImageTypes.includes(image.type)) {
+      setThumbnailPath(image);
+    } else {
+      console.log("Invalid Image type. Please select a PNG, JPG, JPEG image.");
     }
+  }
 
-    const handlePhotoChange = (event) => {
-        const image = event.target.files[0];
-        const allowedImageTypes = ["image/png", "image/jpg", "image/jpeg"];
-        if (image && allowedImageTypes.includes(image.type)) {
-            setThumbnailPath(image);
-        } else {
-            console.log("Invalid Image type. Please select a PNG, JPG, JPEG image.");
-        }
-    };
-
-    return (
-        <Popup contentStyle={{ width: '70%' }}
-            trigger={
-                <button className={styles.add_button} title='Add meal'>
-                    <FaPlus /> Add meal
+  return (
+    <Popup
+      trigger={
+        <button className={styles.add_button}
+          title={t('components.pop_ups.meal_popup.title')}>
+          <FaPlus />{t('components.pop_ups.meal_popup.title')}
+        </button>
+      }
+      modal
+      nested
+    >
+      {close => (
+        <div className={styles.modal}>
+          <button className={styles.close} onClick={close}>
+            &times;
+          </button>
+          <div className={styles.header}>
+            {t('components.pop_ups.meal_popup.header')}
+          </div>
+          <div className={styles.content}>
+            <form
+              className={styles.add_form}
+              onSubmit={(event) => handleNewMeal(event)}
+            >
+              <input
+                type='text'
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+                placeholder={t('components.pop_ups.meal_popup.input1')}
+                className={styles.custom_input1}
+                required
+              />
+              <textarea
+                rows={7}
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+                placeholder={t('components.pop_ups.meal_popup.input2')}
+                className={styles.custom_input1}
+                required
+              />
+              <div className={styles.container}>
+                <input
+                  type="number"
+                  value={calories}
+                  onChange={(event) => setCalories(event.target.value)}
+                  placeholder={t('components.pop_ups.meal_popup.input3')}
+                  step="0.01"
+                  className={styles.custom_input2}
+                  required
+                />
+                <input
+                  type="number"
+                  value={protein}
+                  onChange={(event) => setProtein(event.target.value)}
+                  placeholder={t('components.pop_ups.meal_popup.input4')}
+                  step="0.01"
+                  className={styles.custom_input2}
+                  required
+                />
+                <input
+                  type="number"
+                  value={carbs}
+                  onChange={(event) => setcarbs(event.target.value)}
+                  placeholder={t('components.pop_ups.meal_popup.input5')}
+                  step="0.01"
+                  className={styles.custom_input2}
+                  required
+                />
+                <input
+                  type="number"
+                  value={fat}
+                  onChange={(event) => setfat(event.target.value)}
+                  placeholder={t('components.pop_ups.meal_popup.input6')}
+                  step="0.01"
+                  className={styles.custom_input2}
+                  required
+                />
+              </div>
+              <label htmlFor='thumbnailPath' className={styles.img_holder}>
+                {thumbnailPath ? (
+                  <img
+                    src={URL.createObjectURL(thumbnailPath)}
+                    alt="Uploaded Photo"
+                    style={{ pointerEvents: 'none' }}
+                  />
+                ) : (
+                  <img
+                    src={img_holder}
+                    alt="Photo Placeholder"
+                    style={{ pointerEvents: 'none' }}
+                  />
+                )}
+              </label>
+              <input
+                id='thumbnailPath'
+                type='file'
+                placeholder='Photo'
+                accept='.png,.jpg,.jpeg'
+                onChange={handlePhotoChange}
+                width="100px" height="100px"
+                style={{ visibility: 'hidden' }}
+              />
+              <div className={styles.submit_div}>
+                <button className={styles.submit} type='submit'>
+                  {t('components.pop_ups.meal_popup.title')}
                 </button>
-            }
-            modal
-            nested
-        >
-            {close => (
-                <div className={styles.modal}>
-                    <button className={styles.close} onClick={close}>
-                        &times;
-                    </button>
-                    <div className={styles.header}> Add new meal </div>
-                    <div className={styles.content}>
-                        <form className={styles.add_form} onSubmit={(event) => handleNewMeal(event)}>
-                            <input
-                                type='text'
-                                value={name}
-                                onChange={(event) => setName(event.target.value)}
-                                placeholder='meal name'
-                                className={styles.custom_input}
-                                required
-                            />
-                            <textarea
-                                rows={7}
-                                value={description}
-                                onChange={(event) => setDescription(event.target.value)}
-                                placeholder='meal description'
-                                className={styles.custom_input}
-                                required
-                            />
-                            <input type="number"
-                                value={calories}
-                                onChange={(event) => setCalories(event.target.value)}
-                                placeholder='meal calories per gram'
-                                step="0.01"
-                                className={styles.custom_input}
-                                required />
-                            <input type="number"
-                                value={protein}
-                                onChange={(event) => setProtein(event.target.value)}
-                                placeholder='meal protein per gram'
-                                step="0.01"
-                                className={styles.custom_input}
-                                required />
-                            <input type="number"
-                                value={carbs}
-                                onChange={(event) => setcarbs(event.target.value)}
-                                placeholder='meal carbs per gram'
-                                step="0.01"
-                                className={styles.custom_input}
-                                required />
-                            <input type="number"
-                                value={fat}
-                                onChange={(event) => setfat(event.target.value)}
-                                placeholder='meal fat per gram'
-                                step="0.01"
-                                className={styles.custom_input}
-                                required />
-                            <label htmlFor='thumbnailPath' className={styles.img_holder}>
-                                {thumbnailPath ? (
-                                    <img src={URL.createObjectURL(thumbnailPath)} alt="Uploaded Photo" style={{ pointerEvents: 'none' }} />
-                                ) : (
-                                    <img src={img_holder} alt="Photo Placeholder" style={{ pointerEvents: 'none' }} />
-                                )}
-                            </label>
-                            <input
-                                id='thumbnailPath'
-                                type='file'
-                                placeholder='Photo'
-                                accept='.png,.jpg,.jpeg'
-                                onChange={handlePhotoChange}
-                                width="100px" height="100px"
-                                style={{ visibility: 'hidden' }}
-                            />
-                            <div className={styles.submit_div}>
-                                <button className={styles.submit} type='submit'> Add Meal </button>
-                            </div>
-                        </form>
-                    </div>
-                    <div className={styles.actions}></div>
-                </div>
-            )}
-        </Popup>
-    )
-};
+              </div>
+            </form>
+          </div>
+          <div className={styles.actions}></div>
+        </div>
+      )}
+    </Popup>
+  );
+}
 
 export default NewMealPopUp;
