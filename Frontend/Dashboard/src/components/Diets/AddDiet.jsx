0885bcf2408999a9bf44b4661e99b7bd4c0637d6
ThePhenomenal-Z <alzaintab@gmail.com>
2024-11-23 15:00:52 +0200

    Add diet calculator (better css needer)

--- Frontend/Dashboard/src/components/Diets/AddDiet.jsx
+++ Frontend/Dashboard/src/components/Diets/AddDiet.jsx
@@ -235,7 +235,7 @@ const AddDiet = () => {
           <h2>{t('components.add_diet.h2_1')}</h2>
           <div className={styles.repeat_days_container}>
             <label>
-              {t('components.add_diet.label3')}
+              {t("components.add_diet.label3")}
               <select value={mealsNo} onChange={(event) => setMealsNo(Number(event.target.value))}>
                 {[...Array(7)].map((_, index) => (
                   <option key={index} value={index + 1}>
@@ -245,56 +245,135 @@ const AddDiet = () => {
               </select>
             </label>
             <button onClick={handleSubmit} className={styles.submit_button}>
-              {t('components.add_diet.button3')}
+              {t("components.add_diet.button3")}
             </button>
+
+            {/* Total Nutritional Values */}
+            <div className={styles.totals}>
+              {(() => {
+                // Calculate totals
+                let totalCalories = 0;
+                let totalProtein = 0;
+                let totalCarbs = 0;
+                let totalFat = 0;
+                const giCounts = {};
+
+                meals.forEach((meal) => {
+                  if (meal?.meal_id) {
+                    const selectedMeal = availableMeals.find(
+                      (availableMeal) => availableMeal.meal_id === parseInt(meal.meal_id)
+                    );
+                    if (selectedMeal) {
+                      const quantity = meal.quantity || 0;
+                      totalCalories += selectedMeal.calories_per_gram * quantity;
+                      totalProtein += selectedMeal.protein_per_gram * quantity;
+                      totalCarbs += selectedMeal.carbs_per_gram * quantity;
+                      totalFat += selectedMeal.fat_per_gram * quantity;
+
+                      const giValue = selectedMeal.GI[i18n.language];
+                      if (giValue) {
+                        giCounts[giValue] = (giCounts[giValue] || 0) + 1;
+                      }
+                    }
+                  }
+                });
+
+                // Determine the most frequent GI value
+                const mostFrequentGI = Object.keys(giCounts).reduce((a, b) =>
+                  giCounts[a] > giCounts[b] ? a : b,
+                  ""
+                );
+
+                return (
+                  <div className={styles.totals_info}>
+                    <p>{t("components.add_diet.total_calories")}: {totalCalories.toFixed(2)} kcal</p>
+                    <p>{t("components.add_diet.total_protein")}: {totalProtein.toFixed(2)} g</p>
+                    <p>{t("components.add_diet.total_carbs")}: {totalCarbs.toFixed(2)} g</p>
+                    <p>{t("components.add_diet.total_fat")}: {totalFat.toFixed(2)} g</p>
+                    <p>{t("components.add_diet.most_frequent_gi")}: {mostFrequentGI}</p>
+                  </div>
+                );
+              })()}
+            </div>
           </div>
           <div className={styles.cards_container}>
-            {[...Array(mealsNo)].map((_, mealIndex) => (
-              <div key={mealIndex} className={styles.card}>
-                <label className={styles.card_label}>
-                  {t('components.add_diet.label4')} {mealIndex + 1}:
-                </label>
-                <select
-                  value={meals[mealIndex]?.meal_id || ""}
-                  onChange={(e) => handleMealChange(mealIndex, 'meal_id', e.target.value)}
-                >
-                  <option value="" disabled>{t('components.add_diet.option1')}</option>
-                  {availableMeals.map((meal) => (
-                    <option key={meal.meal_id} value={meal.meal_id}>
-                      {meal.translations[i18n.language].meal_name}
+            {[...Array(mealsNo)].map((_, mealIndex) => {
+              const meal = meals[mealIndex];
+              const selectedMeal = availableMeals.find(
+                (availableMeal) => availableMeal.meal_id === parseInt(meal?.meal_id)
+              );
+              const calories = selectedMeal
+                ? (selectedMeal.calories_per_gram * (meal?.quantity || 0)).toFixed(2)
+                : 0;
+              const protein = selectedMeal
+                ? (selectedMeal.protein_per_gram * (meal?.quantity || 0)).toFixed(2)
+                : 0;
+              const carbs = selectedMeal
+                ? (selectedMeal.carbs_per_gram * (meal?.quantity || 0)).toFixed(2)
+                : 0;
+              const fat = selectedMeal
+                ? (selectedMeal.fat_per_gram * (meal?.quantity || 0)).toFixed(2)
+                : 0;
+              const GI = selectedMeal ? selectedMeal.GI[i18n.language] : "";
+
+              return (
+                <div key={mealIndex} className={styles.card}>
+                  <label className={styles.card_label}>
+                    {t("components.add_diet.label4")} {mealIndex + 1}:
+                  </label>
+                  <select
+                    value={meal?.meal_id || ""}
+                    onChange={(e) => handleMealChange(mealIndex, "meal_id", e.target.value)}
+                  >
+                    <option value="" disabled>
+                      {t("components.add_diet.option1")}
                     </option>
-                  ))}
-                </select>
-                <label className={styles.card_label}>
-                  {t('components.add_diet.quantity_label')}:
-                </label>
-                <input
-                  type="number"
-                  placeholder={t('components.add_diet.quantity')}
-                  value={meals[mealIndex]?.quantity || ""}
-                  onChange={(e) => handleMealChange(mealIndex, 'quantity', e.target.value)}
-                />
-                <label className={styles.card_label}>
-                  {t('components.add_diet.details_label')}:
-                </label>
-                <input
-                  type="text"
-                  placeholder={t('components.add_diet.details')}
-                  value={meals[mealIndex]?.details || ""}
-                  onChange={(e) => handleMealChange(mealIndex, 'details', e.target.value)}
-                />
-                <label className={styles.card_label}>
-                  {t('components.add_diet.time_after_label')}:
-                </label>
-                <input
-                  type="number"
-                  placeholder={t('components.add_diet.time_after')}
-                  value={meals[mealIndex]?.time_after || ""}
-                  onChange={(e) => handleMealChange(mealIndex, 'time_after', e.target.value)}
-                />
-                <span className={styles.hours}>{t('components.add_diet.hours')}</span>
-              </div>
-            ))}
+                    {availableMeals.map((meal) => (
+                      <option key={meal.meal_id} value={meal.meal_id}>
+                        {meal.translations[i18n.language].meal_name}
+                      </option>
+                    ))}
+                  </select>
+                  <label className={styles.card_label}>
+                    {t("components.add_diet.quantity_label")}:
+                  </label>
+                  <input
+                    type="number"
+                    placeholder={t("components.add_diet.quantity")}
+                    value={meal?.quantity || ""}
+                    onChange={(e) => handleMealChange(mealIndex, "quantity", e.target.value)}
+                  />
+                  <label className={styles.card_label}>
+                    {t("components.add_diet.details_label")}:
+                  </label>
+                  <input
+                    type="text"
+                    placeholder={t("components.add_diet.details")}
+                    value={meal?.details || ""}
+                    onChange={(e) => handleMealChange(mealIndex, "details", e.target.value)}
+                  />
+                  <label className={styles.card_label}>
+                    {t("components.add_diet.time_after_label")}:
+                  </label>
+                  <input
+                    type="number"
+                    placeholder={t("components.add_diet.time_after")}
+                    value={meal?.time_after || ""}
+                    onChange={(e) => handleMealChange(mealIndex, "time_after", e.target.value)}
+                  />
+                  <span className={styles.hours}>{t("components.add_diet.hours")}</span>
+                  {selectedMeal && (
+                    <div className={styles.nutrition_info}>
+                      <p>{t("components.add_diet.calories")}: {calories} kcal</p>
+                      <p>{t("components.add_diet.protein")}: {protein} g</p>
+                      <p>{t("components.add_diet.carbs")}: {carbs} g</p>
+                      <p>{t("components.add_diet.fat")}: {fat} g</p>
+                      <p>{t("components.add_diet.gi")}: {GI}</p>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
           </div>
         </>) : (<>
           <h2>{t('components.add_diet.h2_2')}</h2>
