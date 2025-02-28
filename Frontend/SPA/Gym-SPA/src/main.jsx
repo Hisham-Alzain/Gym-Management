b374eaa759cbc664b40cab3dfa04e09071ecd289
Ghassan-123 <ghassankk12342003@gmail.com>
2025-02-28 16:31:44 +0300

    added 3d model react implemintation (model remains)

--- Frontend/SPA/Gym-SPA/src/main.jsx
+++ Frontend/SPA/Gym-SPA/src/main.jsx
@@ -7,6 +7,7 @@ import global_ar from './translations/ar/global.json';
 import { I18nextProvider } from 'react-i18next';
 import i18next from 'i18next';
 import './index.css';
+import Test_3d from './test_3d.jsx';
 
 
 const lng = localStorage.getItem('Lang');
@@ -42,7 +43,8 @@ i18next.init({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18next}>
-      <App />
+      {/* <App /> */}
+      <Test_3d/>
     </I18nextProvider>
   </React.StrictMode>
 );
