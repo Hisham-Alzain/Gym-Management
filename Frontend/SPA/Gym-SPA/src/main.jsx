2b8ab07d8098d637c23194779b23853dc8b1a67a
ThePhenomenal-Z <alzaintab@gmail.com>
2025-03-11 04:03:50 +0300

    some changes SPA

--- Frontend/SPA/Gym-SPA/src/main.jsx
+++ Frontend/SPA/Gym-SPA/src/main.jsx
@@ -7,7 +7,7 @@ import global_ar from './translations/ar/global.json';
 import { I18nextProvider } from 'react-i18next';
 import i18next from 'i18next';
 import './index.css';
-import Test_3d from './test_3d.jsx';
+//import Test_3d from './test_3d.jsx';
 
 
 const lng = localStorage.getItem('Lang');
@@ -43,8 +43,8 @@ i18next.init({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18next}>
-      {/* <App /> */}
-      <Test_3d/>
+      <App />
+      {/* <Test_3d/> */}
     </I18nextProvider>
   </React.StrictMode>
 );
